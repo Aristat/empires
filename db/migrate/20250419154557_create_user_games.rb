8e0676fec7076a6d0cb8a72134a87974c28d4dc6
Alexander Strelnikov <aristat37@gmail.com>
2025-04-19 22:35:07 +0300

    fix user game

--- db/migrate/20250419154557_create_user_games.rb
+++ db/migrate/20250419154557_create_user_games.rb
@@ -3,6 +3,42 @@ class CreateUserGames < ActiveRecord::Migration[8.0]
     create_table :user_games do |t|
       t.references :user, null: false, foreign_key: true
       t.references :game, null: false, foreign_key: true
+      t.references :civilization, null: false, foreign_key: true
+
+      t.integer :turn
+      t.integer :last_turn
+      t.integer :num_turns
+
+      # builders
+      t.integer :food_ratio
+      t.integer :tool_maker
+      t.integer :wood_cutter
+      t.integer :gold_mine
+      t.integer :hunter
+      t.integer :tower
+      t.integer :town_center
+      t.integer :market
+      t.integer :iron_mine
+      t.integer :house
+      t.integer :farmer
+      t.integer :people
+    
+      # land
+      t.integer :f_land
+      t.integer :m_land
+      t.integer :p_land
+    
+      # units
+      t.integer :swordsman
+      t.integer :archers
+      t.integer :horseman
+    
+      # resources
+      t.integer :wood
+      t.integer :food
+      t.integer :iron
+      t.integer :gold
+      t.integer :tools
 
       t.timestamps
     end
