f022af0bda5a6fd68a55e918cff9f85fa5e58cbc
Alexander Strelnikov <aristat37@gmail.com>
2025-04-19 18:47:28 +0300

    init game

---
+++ app/models/game.rb
@@ -0,0 +1,5 @@
+class Game < ApplicationRecord
+  has_and_belongs_to_many :users
+  
+  validates :name, presence: true
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_19_153417) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_19_154557) do
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_games", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_user_games_on_game_id"
+    t.index ["user_id"], name: "index_user_games_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +38,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_19_153417) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "user_games", "games"
+  add_foreign_key "user_games", "users"
 end
