06506293068bceceb51c8384d4b0555608628562
Alexander Strelnikov <aristat37@gmail.com>
2025-04-21 10:30:46 +0300

    fix

--- app/commands/prepare_data_command.rb
+++ app/commands/prepare_data_command.rb
@@ -6,9 +6,31 @@ class PrepareDataCommand < BaseCommand
   end
 
   def call
-    game_data = PrepareGameDataCommand.new(game: user_game.game, civilization: user_game.civilization).call
-    buildings = PrepareBuildingsDataCommand.new(civilization: user_game.civilization).call
+    game_data = PrepareGameDataCommand.new(game: user_game.game, civilization: user_game.civilization).call.with_indifferent_access
+    buildings = PrepareBuildingsDataCommand.new(civilization: user_game.civilization).call.with_indifferent_access
+
+    used_mountains = user_game.iron_mine * buildings[:iron_mine][:settings][:squares] +
+      user_game.gold_mine * buildings[:gold_mine][:settings][:squares]
+    used_forest = user_game.wood_cutter * buildings[:wood_cutter][:settings][:squares] +
+      user_game.hunter * buildings[:hunter][:settings][:squares]
+    used_plains = user_game.farmer * buildings[:farmer][:settings][:squares] +
+      user_game.house * buildings[:house][:settings][:squares] +
+      user_game.market * buildings[:market][:settings][:squares] +
+      user_game.warehouse * buildings[:warehouse][:settings][:squares] +
+      user_game.town_center * buildings[:town_center][:settings][:squares] +
+      user_game.stable * buildings[:stable][:settings][:squares] +
+      user_game.tool_maker * buildings[:tool_maker][:settings][:squares] +
+      user_game.weaponsmith * buildings[:weaponsmith][:settings][:squares] +
+      user_game.fort * buildings[:fort][:settings][:squares] +
+      user_game.tower * buildings[:tower][:settings][:squares] +
+      user_game.winery * buildings[:winery][:settings][:squares] +
+      user_game.mage_tower * buildings[:mage_tower][:settings][:squares]
+
     user_data = {
+      used_mountains: used_mountains,
+      used_forest: used_forest,
+      used_plains: used_plains,
+
       wood_cutter: {
         count: user_game.wood_cutter,
         status: user_game.wood_cutter_status,
@@ -67,11 +89,12 @@ class PrepareDataCommand < BaseCommand
       house: {
         count: user_game.house,
       },
-    }
+    }.with_indifferent_access
+
     {
       game_data: game_data,
       buildings: buildings,
       user_data: user_data
-    }.with_indifferent_access
+    }
   end
 end
