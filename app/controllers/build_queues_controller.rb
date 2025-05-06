31b64690e08a6c592a0b6705989c6b786c70e43e
Alexander Strelnikov <aristat37@gmail.com>
2025-05-06 20:06:46 +0300

    update

--- app/controllers/build_queues_controller.rb
+++ app/controllers/build_queues_controller.rb
@@ -3,12 +3,25 @@ class BuildQueuesController < ApplicationController
   before_action :set_user_game
 
   def create
-    Rails.logger.info([ "params", params ])
+    Rails.logger.info([ "params", build_queue_params ])
+
+    begin
+
+    rescue StandardError => e
+      flash[:alert] = "Error: #{e.message}"
+      Rails.logger.error("Build queue error: #{e.message}")
+    end
 
     redirect_to game_path(@user_game.game)
   end
 
+  private
+
   def set_user_game
     @user_game = current_user.user_games.find(params[:user_game_id])
+  end
+
+  def build_queue_params
+    params.expect(build_queue: [ :queue_type, :building_quantity, :building_type ])
   end
 end
