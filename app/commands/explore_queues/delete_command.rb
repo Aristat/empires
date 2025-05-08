e89c8973d874a5bcba008dd0f45818e0db9e0531
Alexander Strelnikov <aristat37@gmail.com>
2025-05-08 23:36:17 +0300

    update explore queue

--- app/commands/explore_queues/delete_command.rb
+++ app/commands/explore_queues/delete_command.rb
@@ -12,8 +12,36 @@ module ExploreQueues
     end
 
     def call
+      return if failed?
+
+      ActiveRecord::Base.transaction do
+        explore_queues.each do |explore_queue|
+          delete_resources(explore_queue)
+        end
+      end
     rescue StandardError => e
       @errors << e.message
+    end
+
+    private
+
+    def can_cancel?(explore_queue)
+      return false if explore_queue.turns_used.positive?
+      return false if explore_queue.created_at < 15.minutes.ago
+
+      true
+    end
+
+    def delete_resources(explore_queue)
+      return unless can_cancel?(explore_queue)
+
+      @user_game.update!(
+        food: @user_game.food + explore_queue.food,
+        horses: @user_game.horses + explore_queue.horses,
+        people: @user_game.people + explore_queue.people
+      )
+
+      explore_queue.destroy!
     end
   end
 end
