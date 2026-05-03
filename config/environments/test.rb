f3adde21a672dd54840f033aadcabcb0f2f32ae8
Alexander Strelnikov <aristat37@gmail.com>
2026-05-03 12:16:51 +0300

    fix

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Don't raise on missing compiled assets (e.g. tailwind.css) in request specs.
+  config.assets.raise_runtime_errors = false
 end
